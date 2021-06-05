399d9deacf5cf116782e1fd419b47db34379008d
BR88C <bcroethel88@gmail.com>
2021-06-04 20:31:11 -0400

    Add BufferGeometryUtils

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,5 @@
 import * as THREE from '/libs/three.module.js';
+import { BufferGeometryUtils } from '/libs/BufferGeometryUtils.js';
 import { OrbitControls } from '/libs/OrbitControls.js';
 
 let renderer, scene, camera, controls;
